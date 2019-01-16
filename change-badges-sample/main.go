60cf509c9181db0e2fd367a975e1b661fbb832fc
Anatoliy Fedorenko <fedorenko.tolik@gmail.com>
2019-01-16 15:26:48 +0600

    Add copy files functionality. Need to process 'data' param to get repo and branch

--- change-badges-sample/main.go
+++ change-badges-sample/main.go
@@ -4,7 +4,11 @@ import (
 	"context"
 	b64 "encoding/base64"
 	"fmt"
+	"io"
+	"log"
+	"os"
 
+	"cloud.google.com/go/storage"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/sirupsen/logrus"
 )
@@ -31,14 +35,67 @@ func HandleRequest(ctx context.Context, sm SubscriptionMessage) (string, error) 
 		return err.Error(), nil
 	}
 
+	repo := "testRepo"     // with test repo name. Real will be obtained from data after the test with real life data
+	branch := "testBranch" // with test repo branch. Real will be obtained from data after the test with real life data
+
+	filename := fmt.Sprintf("build/%s-%s.svg", repo, branch)
+
+	// Creates a client.
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	// Sets the name for the new bucket.
+	bucketName := "my-new-bucket"
+
+	// Creates a Bucket instance.
+	bucket := client.Bucket(bucketName)
+
 	if sm.Message.Attributes.Status == "SUCCESS" {
 		logrus.Info("Detected build success!")
 
+		f, err := os.Open("build/success.svg")
+		if err != nil {
+			return "", err
+		}
+		defer f.Close()
+
+		wc := bucket.Object(filename).NewWriter(ctx)
+		if _, err = io.Copy(wc, f); err != nil {
+			return "", err
+		}
+		if err := wc.Close(); err != nil {
+			return "", err
+		}
+
+		acl := bucket.Object(filename).ACL()
+		if err := acl.Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
+			return "", err
+		}
 	}
 
 	if sm.Message.Attributes.Status == "FAILURE" {
 		logrus.Info("Detected build failure!")
 
+		f, err := os.Open("build/failure.svg")
+		if err != nil {
+			return "", err
+		}
+		defer f.Close()
+
+		wc := bucket.Object(filename).NewWriter(ctx)
+		if _, err = io.Copy(wc, f); err != nil {
+			return "", err
+		}
+		if err := wc.Close(); err != nil {
+			return "", err
+		}
+
+		acl := bucket.Object(filename).ACL()
+		if err := acl.Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
+			return "", err
+		}
 	}
 
 	return fmt.Sprintf("Build status: %s, data: %s", sm.Message.Attributes.Status, string(sDec)), nil
