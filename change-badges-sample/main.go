c3814c20981b62f0f2f85972f2980a053deec204
Anatoliy Fedorenko <fedorenko.tolik@gmail.com>
2019-01-25 15:55:40 +0600

    (#47) Modify the function to take base64 encoded pubsub messages
    Our GooglePubSub source produces only base64 encoded messages. So,
    this functions takes those messages and decodes them first to str
    and then to json object from which the code would be able to get
    build status of the cloudbuild event.

--- change-badges-sample/main.go
+++ change-badges-sample/main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"context"
-	b64 "encoding/base64"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 
 	"cloud.google.com/go/storage"
@@ -26,38 +27,53 @@ type Attributes struct {
 	Status  string `json: "status"`
 }
 
-func HandleRequest(ctx context.Context, sm SubscriptionMessage) (string, error) {
-	sDec, err := b64.StdEncoding.DecodeString(sm.Message.Data)
+type Base64EncodedPubSubMessage string
+
+func HandleRequest(ctx context.Context, pubsubMessage Base64EncodedPubSubMessage) (string, error) {
+
+	// message := `{"message": {"attributes": {"buildId": "","status": "SUCCESS"}, "data": "SGVsbG8gQ2xvdWQgUHViL1N1YiEgSGVyZSBpcyBteSBtZXNzYWdlIQ==", "message_id": "136969346945"}, "subscription": "projects/myproject/subscriptions/mysubscription"}`
+	// var data SubscriptionMessage
+	// sEnc := base64.StdEncoding.EncodeToString([]byte(message))
+
+	googlePubSubMessage, err := base64.StdEncoding.DecodeString(string(pubsubMessage))
 	if err != nil {
-		return err.Error(), nil
+		logrus.Fatal(err)
 	}
+
+	var data SubscriptionMessage
+	err = json.Unmarshal([]byte(googlePubSubMessage), &data)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	// Need to check if this context is better used
+	//ctx := context.Background()
+
+	// Creates a client.
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		logrus.Fatalf("Failed to create client: %v", err)
+	}
+
+	// Sets the name for the new bucket.
+	bucketName := "anatoliybucket"
+
+	// Creates a Bucket instance.
+	bucket := client.Bucket(bucketName)
 
 	repo := "testRepo"     // with test repo name. Real will be obtained from data after the test with real life data
 	branch := "testBranch" // with test repo branch. Real will be obtained from data after the test with real life data
 
 	filename := fmt.Sprintf("build/%s-%s.svg", repo, branch)
 
-	// Creates a client.
-	client, err := storage.NewClient(ctx)
-	if err != nil {
-		logrus.Fatalf("Failed to create client: %v", err)
-		return "", err
-	}
-
-	// Sets the name for the new bucket.
-	bucketName := "triggermeshtestprojectbucket"
-
-	// Creates a Bucket instance.
-	bucket := client.Bucket(bucketName)
-
-	if sm.Message.Attributes.Status == "SUCCESS" {
+	if data.Message.Attributes.Status == "SUCCESS" {
 		logrus.Info("Detected build success!")
 
 		src := bucket.Object("build/success.svg")
 		dst := bucket.Object(filename)
 
 		if _, err := dst.CopierFrom(src).Run(ctx); err != nil {
-			return "", err
+			logrus.Fatal(err)
 		}
 
 		logrus.Info("Switched badge to build success")
@@ -65,19 +81,18 @@ func HandleRequest(ctx context.Context, sm SubscriptionMessage) (string, error) 
 		acl := bucket.Object(filename).ACL()
 		if err := acl.Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
 			logrus.Error(err)
-			return "", err
 		}
 		logrus.Info("Badge set to public")
 	}
 
-	if sm.Message.Attributes.Status == "FAILURE" {
+	if data.Message.Attributes.Status == "FAILURE" {
 		logrus.Info("Detected build failure!")
 
 		src := bucket.Object("build/failure.svg")
 		dst := bucket.Object(filename)
 
 		if _, err := dst.CopierFrom(src).Run(ctx); err != nil {
-			return "", err
+			logrus.Error(err)
 		}
 
 		logrus.Info("Switched badge to build failure")
@@ -85,17 +100,12 @@ func HandleRequest(ctx context.Context, sm SubscriptionMessage) (string, error) 
 		acl := bucket.Object(filename).ACL()
 		if err := acl.Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
 			logrus.Error(err)
-			return "", err
 		}
 
 		logrus.Info("Badge set to public")
 	}
-
-	return fmt.Sprintf("Build status: %s, data: %s", sm.Message.Attributes.Status, string(sDec)), nil
 }
 
 func main() {
 	lambda.Start(HandleRequest)
 }
-
-//curl http://go-lambda.fedorenkotolik.dev.triggermesh.io -d @testdata.json --header "Content-Type: application/json"
