7660bb3963e9b9231ed438ebbf3996c04ead5bb3
Anatoliy Fedorenko <fedorenko.tolik@gmail.com>
2019-02-21 09:19:30 +0600

    #1 use KLR for spreadsheets example function

--- spreadsheet-sample/main.go
+++ spreadsheet-sample/main.go
@@ -2,24 +2,17 @@ package main
 
 import (
 	"encoding/base64"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"net/http"
 	"os"
 
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/service/dynamodbstreams"
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/sheets/v4"
 )
-
-var spreadsheetID string
-
-type SpreadsheetDumper struct {
-	Sheets *sheets.Service
-}
 
 //DynamoDBEvent represents AWS Dynamo DB payload
 type DynamoDBEvent struct {
@@ -32,13 +25,41 @@ type DynamoDBEvent struct {
 	UserIdentity *dynamodbstreams.Identity     `locationName:"userIdentity" type:"structure"`
 }
 
-func (ssd *SpreadsheetDumper) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	decoder := json.NewDecoder(r.Body)
-	var event DynamoDBEvent
-	err := decoder.Decode(&event)
-	if err != nil {
-		panic(err)
+func Handler(ctx context.Context, event DynamoDBEvent) error {
+
+	spreadsheetID := os.Getenv("SPREADSHEET_ID")
+	_, ok := os.LookupEnv("GOOGLE_APPLICATION_CREDENTIALS")
+
+	if !ok {
+		env := os.Getenv("ENV")
+		fmt.Println(env)
+
+		creds, err := base64.StdEncoding.DecodeString(os.Getenv("CREDENTIALS"))
+		if err != nil {
+			log.Error(err)
+		}
+
+		err = ioutil.WriteFile("credentials.json", []byte(creds), 0644)
+		if err != nil {
+			log.Error(err)
+		}
+
+		os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", "credentials.json")
 	}
+
+	ctx = context.Background()
+
+	client, err := google.DefaultClient(ctx, "https://www.googleapis.com/auth/spreadsheets")
+	if err != nil {
+		return err
+	}
+
+	srv, err := sheets.New(client)
+	if err != nil {
+		return err
+	}
+
+	log.Info("Authorized via service account. Listen for DynamoDB events!")
 
 	var vr sheets.ValueRange
 
@@ -74,48 +95,14 @@ func (ssd *SpreadsheetDumper) ServeHTTP(w http.ResponseWriter, r *http.Request) 
 
 	vr.Values = append(vr.Values, myval)
 
-	_, err = ssd.Sheets.Spreadsheets.Values.Append(spreadsheetID, "A1", &vr).ValueInputOption("RAW").Do()
+	_, err = srv.Spreadsheets.Values.Append(spreadsheetID, "A1", &vr).ValueInputOption("RAW").Do()
 	if err != nil {
-		log.Fatalf("Unable to retrieve data from sheet. %v", err)
+		return err
 	}
 
+	return nil
 }
 
 func main() {
-
-	spreadsheetID = os.Getenv("SPREADSHEET_ID")
-	_, ok := os.LookupEnv("GOOGLE_APPLICATION_CREDENTIALS")
-
-	if !ok {
-		env := os.Getenv("ENV")
-		fmt.Println(env)
-
-		creds, err := base64.StdEncoding.DecodeString(os.Getenv("CREDENTIALS"))
-		if err != nil {
-			log.Error(err)
-		}
-
-		err = ioutil.WriteFile("credentials.json", []byte(creds), 0644)
-		if err != nil {
-			log.Error(err)
-		}
-
-		os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", "credentials.json")
-	}
-
-	ctx := context.Background()
-
-	client, err := google.DefaultClient(ctx, "https://www.googleapis.com/auth/spreadsheets")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	srv, err := sheets.New(client)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Info("Authorized via service account. Listen for DynamoDB events!")
-
-	log.Fatal(http.ListenAndServe(":8080", &SpreadsheetDumper{srv}))
+	lambda.Start(Handler)
 }
